f530385ff5f1a899ac10d4dd230496782d28a835
Israel Gadelha <gdelha.israel@gmail.com>
2021-10-29 20:26:51 -0300

    Feat: Alteraçãoes da Entity City e implementação do PointType
    Alterações feitas na entity City para utlização do Point no atributo lat_lon. Criado também a classe personalizada PointType.

--- src/main/java/com/github/israelgda/citiesapi/entities/City.java
+++ src/main/java/com/github/israelgda/citiesapi/entities/City.java
@@ -1,11 +1,20 @@
 package com.github.israelgda.citiesapi.entities;
 
+import com.github.israelgda.citiesapi.utils.PointType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import org.springframework.data.geo.Point;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
 @Table(name="cidade")
+@TypeDefs(value = {
+        @TypeDef(name = "point", typeClass = PointType.class)
+})
 public class City implements Serializable {
 
     //Attributes
@@ -20,14 +29,18 @@ public class City implements Serializable {
     @Column(name="ibge", nullable = false)
     private Integer ibge;
 
-    @Column(name="lat_lon", nullable = false)
-    private Double lat_lon;
+//    @Column(name="lat_lon", nullable = false)
+//    private String lat_lon;
+
+    @Type(type = "point")
+    @Column(name = "lat_lon", updatable = false, insertable = false)
+    private Point lat_lon;
 
     //Constructors
     public City() {
     }
 
-    public City(long id, Integer uf, Integer ibge, Double lat_lon) {
+    public City(long id, Integer uf, Integer ibge, Point lat_lon) {
         this.id = id;
         this.uf = uf;
         this.ibge = ibge;
@@ -59,13 +72,21 @@ public class City implements Serializable {
         this.ibge = ibge;
     }
 
-    public Double getLat_lon() {
+    public Point getLat_lon() {
         return lat_lon;
     }
 
-    public void setLat_lon(Double lat_lon) {
+    public void setLat_lon(Point lat_lon) {
         this.lat_lon = lat_lon;
     }
+
+//    public String getLat_lon() {
+//        return lat_lon;
+//    }
+//
+//    public void setLat_lon(String lat_lon) {
+//        this.lat_lon = lat_lon;
+//    }
 
     //HashCode and Equals
     @Override
