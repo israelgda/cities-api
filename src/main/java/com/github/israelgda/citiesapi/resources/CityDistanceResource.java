58a6326b0b1d1994b09a164b2e442fbfd1c9f0ca
Israel Gadelha <gdelha.israel@gmail.com>
2021-10-31 15:34:25 -0300

    Fix: Correção do tipo de retorno do Resource CityDistance
    O tipo de retorno foi corrigido e adequado para ResponseEntity.

--- src/main/java/com/github/israelgda/citiesapi/resources/CityDistanceResource.java
+++ src/main/java/com/github/israelgda/citiesapi/resources/CityDistanceResource.java
@@ -1,6 +1,7 @@
 package com.github.israelgda.citiesapi.resources;
 
 import com.github.israelgda.citiesapi.services.CityDistanceService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +18,14 @@ public class CityDistanceResource {
     }
 
     @GetMapping("/by-points")
-    public Double byPoints(@RequestParam(name = "from") final Long city1,
-                           @RequestParam(name = "to") final Long city2) {
-        return service.distanceByPointsInMiles(city1, city2);
+    public ResponseEntity byPoints(@RequestParam(name = "from") final Long city1,
+                                   @RequestParam(name = "to") final Long city2) {
+        return ResponseEntity.ok().body(service.distanceByPointsInMiles(city1, city2));
     }
 
     @GetMapping("/by-cube")
-    public Double byCube(@RequestParam(name = "from") final Long city1,
+    public ResponseEntity byCube(@RequestParam(name = "from") final Long city1,
                          @RequestParam(name = "to") final Long city2) {
-        return service.distanceByCubeInMeters(city1, city2);
+        return ResponseEntity.ok().body(service.distanceByCubeInMeters(city1, city2));
     }
 }
