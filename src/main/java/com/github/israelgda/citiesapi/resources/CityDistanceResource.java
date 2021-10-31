7c7ed12f80ad421e78814be3cf7ff68f550d7768
Israel Gadelha <gdelha.israel@gmail.com>
2021-10-31 15:23:03 -0300

    Fix: Remoção de dependências não utilizadas
    ...

--- src/main/java/com/github/israelgda/citiesapi/resources/CityDistanceResource.java
+++ src/main/java/com/github/israelgda/citiesapi/resources/CityDistanceResource.java
@@ -1,8 +1,6 @@
 package com.github.israelgda.citiesapi.resources;
 
 import com.github.israelgda.citiesapi.services.CityDistanceService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
